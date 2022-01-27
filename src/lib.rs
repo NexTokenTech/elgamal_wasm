f34c93ccdda0b29da4217c8db437095607777362
kaisuki <1103720832@qq.com>
2022-01-27 11:02:25 +0800

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+mod elgamal {
+    pub fn test_fn(){
+        println!("test function suc");
+    }
+}
